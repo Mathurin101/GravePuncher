8b6d95a7bdd7ff37d738e28f1c08c60a5a7b26db
Mathurin.G <mathuringenty101@gmail.com>
2026-01-26 01:24:09 -0500

    added jump method
    Jump method in the player script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] int Speed = 1;
     [SerializeField] int Hearts = 3;
     [SerializeField] int TotalJumps = 1;
+    [SerializeField] int JumpMax = 1;
+    [SerializeField] int JumpSpeed = 2;
+
 
     [Header("MISC")]
     [SerializeField] float Gravity = 9.8f;
@@ -15,18 +18,18 @@ public class PlayerController : MonoBehaviour
     //Original Stats
     int PlayerOGHearts;
     int PlayerOGSpeed;
+    int OGGravity;
 
     //Counters
     int JumpCount;
 
-
     Vector3 MoveDirection;
-
+    Vector3 JumpVelocity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        OGGravity = (int)Gravity;
     }
 
     // Update is called once per frame
@@ -38,33 +41,38 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
-   
-        MoveDirection = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-        controller.Move(MoveDirection * Speed * Time.deltaTime);
-        /*switch (KeyCode.At)
+        if (controller.isGrounded)
         {
-            case KeyCode.A:
+            JumpVelocity = Vector3.zero;
+            JumpCount = 0;
+            Gravity = OGGravity;
+        }
+        else
+        {
+            //Apply Gravity
+            JumpVelocity.y -= (Gravity * Time.deltaTime);
+        }
+            MoveDirection = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(MoveDirection * Speed * Time.deltaTime);
 
-            case KeyCode.D:
+        //if you wanted to try to make it move one direction at a time
 
-                MoveDirection = Input.GetAxis("Horizontal") * transform.right;
-                controller.Move(MoveDirection * Speed * Time.deltaTime);
-                break;
-
-            case KeyCode.W:
-
-            case KeyCode.S:
-                MoveDirection = Input.GetAxis("Vertical") * transform.forward;
-                controller.Move(MoveDirection * Speed * Time.deltaTime);
-                break;
-        }*/
-
-
+        Jump();
+        controller.Move(JumpVelocity * Time.deltaTime);
     }
 
-    //punch
-
     //jump
+    void Jump()
+    {
+        if (Input.GetButton("Jump") && JumpCount <= JumpMax)
+        {
+            JumpVelocity.y = JumpSpeed;
+            JumpCount++;
+        }
+    }
+
+
+    //punch
 
     //jumpPunch
 
